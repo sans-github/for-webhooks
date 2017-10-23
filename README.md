d2d4d17975b824b5ed38fe4b062fc95ce6718749
San <San>
2017-10-22 22:06:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+#  Running the app
+    java -jar for-webhooks-0.0.1-SNAPSHOT.jar
+
+# API
+## Request #1  
+    curl -sk http://localhost:58080/hello -H "Content-Type: application/json"
+     
+    hello there
+    
+## Request #2
+    curl -sk -X POST http://localhost:58080/webhooks -H "Content-Type: application/json"
+     
+    merchant_account_id not available
+    serializedObject={ }
+
+ ## Request #3
+     curl -sk -X POST http://localhost:58080/webhooks?merchant_account_id=abcd1234 -H "Content-Type: application/json"
+     
+     merchant_account_id=abcd1234
+     serializedObject={
+       "merchant_account_id" : [ "abcd1234" ]
+     }
+     
+ ## Request #4
+     curl -sk -X POST http://localhost:58080/webhooks/abcd1234 -H "Content-Type: application/json"
+     
+     merchant_account_id=abcd1234
+     serializedObject={ }
+
+## Request #5
+     curl -sk -X POST http://localhost:58080/object -H "Content-Type: application/json" -d "{\"greeting\": \"Hello\", \"name\": \"Cowie\"}"
+      
+      {
+          "greeting": "Hello",
+          "name": "Cowie"
+      }
+     
+     
---
+++ src/main/java/com/spring/boot/controller/WebhooksController.java
@@ -0,0 +1,77 @@
+package com.spring.boot.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@RestController
+@RequestMapping(value = "/", produces = "application/json")
+public class WebhooksController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebhooksController.class);
+
+    @RequestMapping(value = "/hello", method = GET)
+    @ResponseBody
+    public String hello() {
+        return "hello there\n";
+    }
+
+
+    @RequestMapping(value = "/webhooks", method = POST)
+    public void handleWebhooks(final HttpServletRequest httpServletRequest) {
+
+        final String merchantAccountId = httpServletRequest.getParameter("merchant_account_id");
+        if (StringUtils.hasText(merchantAccountId)) {
+            LOGGER.info("merchant_account_id={}", merchantAccountId);
+        } else {
+            LOGGER.error("merchant_account_id not available");
+        }
+
+        printObject(httpServletRequest.getParameterMap());
+    }
+
+    @RequestMapping(value = "/webhooks/{merchant_account_id}", method = POST)
+    public void handle(@PathVariable("merchant_account_id") @NotEmpty final String merchantAccountId, final HttpServletRequest httpServletRequest) {
+
+        LOGGER.info("merchant_account_id={}", merchantAccountId);
+
+        printObject(httpServletRequest.getParameterMap());
+    }
+
+    @RequestMapping(value = "/object", method = POST, consumes = APPLICATION_JSON_VALUE)
+    public String handleAnyJson(final InputStream inputStream) throws IOException {
+        final String jsonInRequest = IOUtils.toString(inputStream, Charset.defaultCharset());
+        LOGGER.info("jsonInRequest={}", jsonInRequest);
+        return jsonInRequest;
+    }
+
+    private static void printObject(final Object object) {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(INDENT_OUTPUT);
+
+        try {
+            LOGGER.info("serializedObject={}", objectMapper.writeValueAsString(object));
+        } catch (final JsonProcessingException ex) {
+            LOGGER.error("Error={}", ex.getMessage());
+        }
+
+    }
+}
